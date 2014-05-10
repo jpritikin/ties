dd3b9082002d7359a7bdca470a2531c62dc987b6
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2014-05-10 15:54:43 +0530

    3 latent

--- germano2014/ct.R
+++ germano2014/ct.R
@@ -11,7 +11,7 @@ w1end <- strptime(w1$end, "%m/%d/%Y %H:%M:%S")
 w2end <- strptime(w2$end, "%m/%d/%Y %H:%M:%S")
 w3end <- strptime(w3$end, "%m/%d/%Y %H:%M:%S")
 
-manifests <- colnames(w1)[8]  # 8:17
+manifests <- colnames(w1)[15:17]  # 8:17
 
 replaceNA <- function (v, with=-1) {
   v[is.na(v)] <- with
@@ -35,22 +35,19 @@ latentM1  <- matrix(apply(w1[,manifests, drop=FALSE],2,mean, na.rm=TRUE), nrow=n
 
 manifestM <- matrix(0, nrow=n.manifest, ncol=1)
 
-G    	<- matrix(.1, 1,1)
+G    	<- matrix(.1, n.latent, n.latent)
 G[upper.tri(G)] <- 0
 
-drift <- matrix(-0.1, n.latent, n.latent)
+drift <- .1 - diag(n.latent)
 
 THETA    <- matrix(0									# var/cov matrix of measurement error
                    ,nrow=n.manifest, ncol=n.manifest)
 
 fit <- continuous_time(ctdat, Tpoints=3, n.latent, n.manifest,
-                       LAMBDA=matrix(1), DRIFT=drift,
+                       LAMBDA=diag(3), #manifest to latent loading
+                       DRIFT=drift,
                        CINT=manifestM,
-                       G=G, D=80,
+                       G=G, D=5,
                        PHI1,
-                       THETA=THETA, latentM1, manifestM=matrix(0))
+                       THETA=THETA, latentM1, manifestM=manifestM)
 
-omxCheckCloseEnough(fit$output$fit, 1114, 1)
-
-# fit <- continuous_time(ctdat, 3, n.latent, drift, manifestM, G, oversamples, n.manifest,
-#                        diag(n.latent), PHI1, THETA, latentM1, manifestM)
