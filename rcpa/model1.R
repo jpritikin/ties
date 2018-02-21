9a2d071d9344a77b58f9d18081d80776a5466790
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-02-21 17:06:27 -0500

    Add independence model

---
+++ rcpa/model1.R
@@ -0,0 +1,69 @@
+library(rstan)
+library(jsonlite)
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+rcd <- read.csv("rawData.csv")
+rcd <- rcd[,-match(c('recno'), colnames(rcd))]
+
+if (nrow(rcd) < 1) { stop("No data?") }
+facetNames <- colnames(rcd[-1:-4])
+NFACETS <- length(facetNames)
+
+palist <- sort(unique(c(as.character(rcd$pa1), as.character(rcd$pa2))))
+NPA <- length(palist)
+
+spokes <- rep(NA, length(palist))
+names(spokes) <- palist
+for (pa1 in palist) {
+    other <- c()
+    for (side in 1:2) {
+        col1 <- paste0('pa', side)
+        col2 <- paste0('pa', 3-side)
+        other <- c(other, as.character(rcd[rcd[[col1]] == pa1, col2]))
+    }
+    spokes[pa1] <- sum(!duplicated(other))
+}
+
+# Otherwise the priors are given too much influence
+whitelist <- names(spokes)[spokes >= 3]
+rcd <- rcd[rcd$pa1 %in% whitelist & rcd$pa2 %in% whitelist,]
+
+#soloGroupList <- which(duplicated(sub(";(solo|group)$", "", palist)))
+
+rcd[is.na(rcd)] <- 10
+
+sim_fit <- stan(file = "model1.stan",
+                data = list(NPA=NPA, NFACETS=NFACETS, NCMP=nrow(rcd),
+                            pa1=match(rcd$pa1, palist),
+                            pa2=match(rcd$pa2, palist),
+                            diff=sapply(rcd[-1:-4], as.numeric)),
+                chains = 6,
+                iter = 500,
+                control = list(max_treedepth = 15))
+
+facetNames <- colnames(rcd[-1:-4])
+
+save(sim_fit, facetNames, rcd, spokes, NPA, NFACETS, whitelist, file="/tmp/simFit1.rda")
+if (0) {
+    load("/tmp/simFit1.rda")
+}
+
+divergent <- get_sampler_params(sim_fit, inc_warmup=FALSE)[[1]][,'divergent__']
+print(sum(divergent))
+
+neOrder <- order(summary(sim_fit)$summary[,c('n_eff')])
+head(summary(sim_fit, probs=.5)$summary[neOrder,], n=20)
+
+estimator <- 'mean'
+df <- summary(sim_fit, pars=c("alpha"), probs=.5)$summary
+alpha <- matrix(df[,estimator], nrow=1,
+                dimnames=list(NULL, facetNames))
+print(alpha)
+
+df <- summary(sim_fit, pars=c("theta"), probs=.5)$summary
+tar <- array(df[,estimator], dim=c(NFACETS, length(whitelist)))
+
+# Practically impossible to overcome prior mean at zero
+hist(tar, breaks=100)
---
+++ rcpa/model1.stan
@@ -0,0 +1,55 @@
+functions {
+  vector cmp_probs(real alpha, real pa1, real pa2, real thr1, real thr2) {
+    vector[5] unsummed;
+    real paDiff = (pa1 - pa2);
+    unsummed[1] = 0;
+    unsummed[2] = paDiff - (thr1 + thr2);
+    unsummed[3] = paDiff - thr1;
+    unsummed[4] = paDiff + thr1;
+    unsummed[5] = paDiff + thr1 + thr2;
+    return cumulative_sum(alpha * unsummed);
+  }
+}
+data {
+  // dimensions
+  int<lower=1> NPA;             // # physical activites
+  int<lower=1> NFACETS;         // # facets (i.e. characteristics)
+  int<lower=1> NCMP;            // # comparisons (sample size)
+  // actual data follows
+  int<lower=1, upper=NPA> pa1[NCMP];        // PA1 for observation N
+  int<lower=1, upper=NPA> pa2[NCMP];        // PA2 for observation N
+  int<lower=-2, upper=10> diff[NCMP,NFACETS];   // comparisons
+}
+transformed data {
+  int rcat[NCMP,NFACETS];
+
+  for (cmp in 1:NCMP) {
+    for (ff in 1:NFACETS) {
+      rcat[cmp,ff] = diff[cmp,ff] + 3;
+    }
+  }
+}
+parameters {
+  matrix[NPA,NFACETS]     theta;    // latent score of PA by facet
+  real threshold1;
+  real threshold2;
+  vector<lower=0>[NFACETS] alpha;
+}
+model {
+  for (pa in 1:NPA) {
+    theta[pa,] ~ normal(0,1);
+  }
+  threshold1 ~ normal(0,5);
+  threshold2 ~ normal(0,5);
+  alpha ~ lognormal(0, 1);
+  for (cmp in 1:NCMP) {
+    for (ff in 1:NFACETS) {
+      if (rcat[cmp,ff] == 13) continue;  // special value to indicate missing
+      rcat[cmp,ff] ~ categorical_logit(
+        cmp_probs(alpha[ff],
+          theta[pa1[cmp],ff],
+          theta[pa2[cmp],ff],
+          threshold1, threshold2));
+    }
+  }
+}
