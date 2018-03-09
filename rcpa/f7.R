9e62693acf87e6b539016aad1317b27a576a2ea6
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-03-09 08:15:44 -0500

    Tune sampler settings

--- rcpa/f7.R
+++ rcpa/f7.R
@@ -5,9 +5,9 @@ rcd <- loadSingleFactorData()
 fit2t5 <- stan(file = "model3.stan",
                data = prepDataForStan(rcd),
                chains = stanChains,
-               iter = 1000,
+               iter = 2000,
                include=FALSE,
                pars=c('theta_raw', 'rawFlow', 'rawLoadings'),
-               control = list(max_treedepth = 15))
+               control = list(max_treedepth = 15, adapt_delta=.95))
 
 save(fit2t5, rcd, file=paste0(outputDir(), "fit2t5.rda"))
