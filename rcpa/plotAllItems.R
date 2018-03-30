7f4eddd2eb6c93141fa6a0c2bf167ae982aeba79
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2018-03-30 13:26:13 -0400

    Text complete

---
+++ rcpa/plotAllItems.R
@@ -0,0 +1,30 @@
+# independence model
+# all data vs tiny sample size activities removed
+
+source("modelUtil.R")
+
+#colSums(is.na(rcd[-1:-4]))  # seems fairly uniform TODO
+
+load(paste0(outputDir(), "fit1s1.rda"))
+
+facetNames <- extractFacetNames(rcd)
+palist <- extractPalist(rcd)
+
+estimator <- 'mean'
+
+df <- summary(fit1s1, pars=c("theta"), probs=c())$summary
+tar1 <- array(df[,estimator], dim=c(length(facetNames), length(palist)))
+
+load(paste0(outputDir(), "fit1s2.rda"))
+
+whitelist <- extractPalist(rcd)
+
+df <- summary(fit1s2, pars=c("theta"), probs=c())$summary
+tar2 <- array(df[,estimator], dim=c(length(facetNames), length(whitelist)))
+
+hist(c(tar1), breaks = 100)
+hist(c(tar2), breaks = 100)
+
+cor(c(tar1[,match(whitelist, palist)]), c(tar2))
+
+plotByFacet(fit1s2, rcd)
